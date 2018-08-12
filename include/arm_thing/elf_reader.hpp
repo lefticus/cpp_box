214500e0fb48ae1312950dbdc19fcb97ed35a00e
Jason Turner <jason@emptycrate.com>
2018-08-11 19:14:10 -0600

    elf reader working

--- include/arm_thing/elf_reader.hpp
+++ include/arm_thing/elf_reader.hpp
@@ -1,32 +1,220 @@
 #include <array>
 #include <cassert>
+#include <iostream>
 
 namespace arm_thing::ELF {
+
+// todo, move this into a shared utility location
+template<std::size_t Bytes, typename Data>
+[[nodiscard]] constexpr auto read_loc(const Data &data, const std::size_t loc, [[maybe_unused]] const bool little_endian) noexcept
+{
+  // to do : assert size is within bounds?
+  if constexpr (Bytes == 1) {
+    return static_cast<std::uint8_t>(data[loc]);
+  } else if constexpr (Bytes == 2) {
+    const std::uint16_t byte0 = data[loc];
+    const std::uint16_t byte1 = data[loc + 1];
+    if (little_endian) {
+      return static_cast<std::uint16_t>(byte0 | (byte1 << 8));
+    } else {
+      return static_cast<std::uint16_t>((byte0 << 8) | byte1);
+    }
+  } else if constexpr (Bytes == 4) {
+    const std::uint32_t byte0 = data[loc];
+    const std::uint32_t byte1 = data[loc + 1];
+    const std::uint32_t byte2 = data[loc + 2];
+    const std::uint32_t byte3 = data[loc + 3];
+    if (little_endian) {
+      return byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24);
+    } else {
+      return (byte0 << 24) | (byte1 << 16) | (byte2 << 8) | byte3;
+    }
+  } else if constexpr (Bytes == 8) {
+    const std::uint64_t byte0 = data[loc];
+    const std::uint64_t byte1 = data[loc + 1];
+    const std::uint64_t byte2 = data[loc + 2];
+    const std::uint64_t byte3 = data[loc + 3];
+    const std::uint64_t byte4 = data[loc + 4];
+    const std::uint64_t byte5 = data[loc + 5];
+    const std::uint64_t byte6 = data[loc + 6];
+    const std::uint64_t byte7 = data[loc + 7];
+    if (little_endian) {
+      return byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24) | (byte4 << 32) | (byte5 << 40) | (byte6 << 48) | (byte7 << 56);
+    } else {
+      return (byte0 << 56) | (byte1 << 48) | (byte2 << 40) | (byte3 << 32) | (byte4 << 24) | (byte5 << 16) | (byte6 << 8) | byte7;
+    }
+  } else {
+    static_assert(Bytes == 1 || Bytes == 2 || Bytes == 4 || Bytes == 8);
+  }
+}
+
+template<typename Getter>[[nodiscard]] constexpr auto make_iterator(Getter t_getter, const std::size_t t_max) noexcept
+{
+  struct Const_Iterator
+  {
+    std::size_t idx{};
+    Getter getter;
+    [[nodiscard]] constexpr auto operator*() const noexcept { return getter(idx); }
+    constexpr Const_Iterator &operator++() noexcept
+    {
+      ++idx;
+      return *this;
+    }
+    [[nodiscard]] constexpr bool operator!=(const Const_Iterator &other) const noexcept { return idx != other.idx; }
+  };
+
+  struct Iterable
+  {
+    [[nodiscard]] constexpr Const_Iterator begin() const noexcept { return Const_Iterator{ 0, getter }; }
+    [[nodiscard]] constexpr Const_Iterator end() const noexcept { return Const_Iterator{ max, getter }; }
+
+    std::size_t max{};
+    Getter getter;
+  };
+
+  return Iterable{ t_max, t_getter };
+}
+
+
+// https://docs.oracle.com/cd/E23824_01/html/819-0690/chapter6-79797.html#chapter6-tbl-23
+struct Symbol_Table_Entry
+{
+  enum class Fields { st_name, st_value, st_size, st_info, st_other, st_shndx };
+
+  enum class Binding {
+    STB_LOCAL  = 0,
+    STB_GLOBAL = 1,
+    STB_WEAK   = 2,
+    STB_LOOS   = 10,
+    STB_HIOS   = 12,
+    STB_LOPROC = 13,
+    STB_HIPROC = 15,
+  };
+
+  enum class Type {
+    STT_NOTYPE         = 0,
+    STT_OBJECT         = 1,
+    STT_FUNC           = 2,
+    STT_SECTION        = 3,
+    STT_FILE           = 4,
+    STT_COMMON         = 5,
+    STT_TLS            = 6,
+    STT_LOOS           = 10,
+    STT_HIOS           = 12,
+    STT_LOPROC         = 13,
+    STT_SPARC_REGISTER = 13,
+    STT_HIPROC         = 15
+  };
+
+
+  enum class Visibility {
+    STV_DEFAULT   = 0,
+    STV_INTERNAL  = 1,
+    STV_HIDDEN    = 2,
+    STV_PROTECTED = 3,
+    STV_EXPORTED  = 4,
+    STV_SINGLETON = 5,
+    STV_ELIMINATE = 6
+  };
+
+  bool bits_32       = true;
+  bool little_endian = true;
+  std::basic_string_view<std::uint8_t> full_data;
+  std::basic_string_view<std::uint8_t> data;
+
+  [[nodiscard]] constexpr static auto entry_size(const bool t_bits_32) noexcept -> std::size_t
+  {
+    if (t_bits_32) {
+      return 16;
+    } else {
+      return 24;
+    }
+  }
+
+  constexpr Symbol_Table_Entry(const bool t_bits_32,
+                               const bool t_little_endian,
+                               const std::basic_string_view<std::uint8_t> t_full_data,
+                               const std::basic_string_view<std::uint8_t> t_data) noexcept
+    : bits_32{ t_bits_32 }, little_endian{ t_little_endian }, full_data{ t_full_data }, data{ t_data }
+  {
+  }
+
+  [[nodiscard]] constexpr auto offset(const Fields field) const noexcept -> std::size_t
+  {
+    switch (field) {
+    case Fields::st_name: return 0;
+    case Fields::st_value: return bits_32 ? 4 : 8;
+    case Fields::st_size: return bits_32 ? 8 : 16;
+    case Fields::st_info: return bits_32 ? 12 : 4;
+    case Fields::st_other: return bits_32 ? 13 : 5;
+    case Fields::st_shndx: return bits_32 ? 14 : 6;
+    }
+  }
+
+  [[nodiscard]] constexpr auto size(const Fields field) const noexcept
+  {
+    switch (field) {
+    case Fields::st_name: return 4;
+    case Fields::st_value: return bits_32 ? 4 : 8;
+    case Fields::st_size: return bits_32 ? 4 : 8;
+    case Fields::st_info: return 1;
+    case Fields::st_other: return 1;
+    case Fields::st_shndx: return 2;
+    }
+  }
+
+  [[nodiscard]] constexpr auto name_offset() const noexcept { return read(Fields::st_name); }
+
+  [[nodiscard]] constexpr auto section_header_table_index() const noexcept { return read(Fields::st_shndx); }
+
+  [[nodiscard]] constexpr auto name(const std::string_view string_table) const noexcept -> std::string_view
+  {
+    const auto name_loc   = name_offset();
+    const auto name_start = string_table.substr(name_loc);
+    return name_start.substr(0, name_start.find('\0'));
+  }
+
+  [[nodiscard]] constexpr auto value() const noexcept { return read(Fields::st_value); }
+
+  [[nodiscard]] constexpr auto size() const noexcept { return read(Fields::st_size); }
+
+
+  [[nodiscard]] constexpr auto read(const Fields field) const noexcept -> std::uint64_t
+  {
+    switch (size(field)) {
+    case 1: return read_loc<1>(data, offset(field), true);
+    case 2: return read_loc<2>(data, offset(field), little_endian);
+    case 4: return read_loc<4>(data, offset(field), little_endian);
+    case 8: return read_loc<8>(data, offset(field), little_endian);
+    }
+  }
+};
 
 struct Section_Header
 {
   enum class Fields { sh_name, sh_type, sh_flags, sh_addr, sh_offset, sh_size, sh_link, sh_info, sh_addralign, sh_entsize };
 
   enum class Types {
-    SHT_NULL          = 0x00,       // Section header table entry unused
-    SHT_PROGBITS      = 0x01,       // Program data
-    SHT_SYMTAB        = 0x02,       // Symbol table
-    SHT_STRTAB        = 0x03,       // String table
-    SHT_RELA          = 0x04,       // Relocation entries with addends
-    SHT_HASH          = 0x05,       // Symbol hash table
-    SHT_DYNAMIC       = 0x06,       // Dynamic linking information
-    SHT_NOTE          = 0x07,       // Notes
-    SHT_NOBITS        = 0x08,       // Program space with no data (bss)
-    SHT_REL           = 0x09,       // Relocation entries, no addends
-    SHT_SHLIB         = 0x0A,       // Reserved
-    SHT_DYNSYM        = 0x0B,       // Dynamic linker symbol table
-    SHT_INIT_ARRAY    = 0x0E,       // Array of constructors
-    SHT_FINI_ARRAY    = 0x0F,       // Array of destructors
-    SHT_PREINIT_ARRAY = 0x10,       // Array of pre-constructors
-    SHT_GROUP         = 0x11,       // Section group
-    SHT_SYMTAB_SHNDX  = 0x12,       // Extended section indices
-    SHT_NUM           = 0x13,       // Number of defined types.
-    SHT_LOOS          = 0x60000000  // Start OS-specifica.
+    SHT_NULL          = 0x00,        // Section header table entry unused
+    SHT_PROGBITS      = 0x01,        // Program data
+    SHT_SYMTAB        = 0x02,        // Symbol table
+    SHT_STRTAB        = 0x03,        // String table
+    SHT_RELA          = 0x04,        // Relocation entries with addends
+    SHT_HASH          = 0x05,        // Symbol hash table
+    SHT_DYNAMIC       = 0x06,        // Dynamic linking information
+    SHT_NOTE          = 0x07,        // Notes
+    SHT_NOBITS        = 0x08,        // Program space with no data (bss)
+    SHT_REL           = 0x09,        // Relocation entries, no addends
+    SHT_SHLIB         = 0x0A,        // Reserved
+    SHT_DYNSYM        = 0x0B,        // Dynamic linker symbol table
+    SHT_INIT_ARRAY    = 0x0E,        // Array of constructors
+    SHT_FINI_ARRAY    = 0x0F,        // Array of destructors
+    SHT_PREINIT_ARRAY = 0x10,        // Array of pre-constructors
+    SHT_GROUP         = 0x11,        // Section group
+    SHT_SYMTAB_SHNDX  = 0x12,        // Extended section indices
+    SHT_NUM           = 0x13,        // Number of defined types.
+    SHT_LOOS          = 0x60000000,  // Start OS-specific.
+    Unknown
   };
 
   enum class Flags : std::uint32_t {
@@ -44,13 +232,20 @@ struct Section_Header
     SHF_MASKPROC         = 0xf0000000,  // Processor-specific
     SHF_ORDERED          = 0x4000000,   // Special ordering requirement (Solaris)
     SHF_EXCLUDE          = 0x8000000    // Section is excluded unless referenced or allocated (Solaris)
-
   };
 
-  bool bits_32 = true;
-  std::array<std::uint8_t, 0x40> data;
+  bool bits_32       = true;
+  bool little_endian = true;
+  std::basic_string_view<std::uint8_t> full_data;
+  std::basic_string_view<std::uint8_t> data;
 
-  Section_Header(const bool t_bits_32) : bits_32{ t_bits_32 } {}
+  constexpr Section_Header(const bool t_bits_32,
+                           const bool t_little_endian,
+                           const std::basic_string_view<std::uint8_t> t_full_data,
+                           const std::basic_string_view<std::uint8_t> t_data)
+    : bits_32{ t_bits_32 }, little_endian{ t_little_endian }, full_data{ t_full_data }, data{ t_data }
+  {
+  }
 
   [[nodiscard]] constexpr auto offset(const Fields field) const noexcept -> std::size_t
   {
@@ -83,6 +278,82 @@ struct Section_Header
     case Fields::sh_entsize: return bits_32 ? 4 : 8;
     }
   }
+
+  [[nodiscard]] constexpr auto type() const noexcept -> Types
+  {
+    switch (const auto val = static_cast<Types>(read(Fields::sh_type)); val) {
+    case Types::SHT_NULL:
+    case Types::SHT_PROGBITS:
+    case Types::SHT_SYMTAB:
+    case Types::SHT_STRTAB:
+    case Types::SHT_RELA:
+    case Types::SHT_HASH:
+    case Types::SHT_DYNAMIC:
+    case Types::SHT_NOTE:
+    case Types::SHT_NOBITS:
+    case Types::SHT_REL:
+    case Types::SHT_SHLIB:
+    case Types::SHT_DYNSYM:
+    case Types::SHT_INIT_ARRAY:
+    case Types::SHT_FINI_ARRAY:
+    case Types::SHT_PREINIT_ARRAY:
+    case Types::SHT_GROUP:
+    case Types::SHT_SYMTAB_SHNDX:
+    case Types::SHT_NUM:
+    case Types::SHT_LOOS:
+    case Types::Unknown: return val;
+    }
+
+    return Types::Unknown;
+  }
+
+  [[nodiscard]] constexpr auto name_offset() const noexcept { return read(Fields::sh_name); }
+
+  [[nodiscard]] constexpr auto offset() const noexcept { return read(Fields::sh_offset); }
+
+  [[nodiscard]] constexpr auto name(const std::string_view string_table) const noexcept -> std::string_view
+  {
+    const auto name_loc   = name_offset();
+    const auto name_start = string_table.substr(name_loc);
+    return name_start.substr(0, name_start.find('\0'));
+  }
+
+  [[nodiscard]] constexpr auto size() const noexcept { return read(Fields::sh_size); }
+
+  [[nodiscard]] constexpr auto read(const Fields field) const noexcept -> std::uint64_t
+  {
+    switch (size(field)) {
+    case 1: return read_loc<1>(data, offset(field), true);
+    case 2: return read_loc<2>(data, offset(field), little_endian);
+    case 4: return read_loc<4>(data, offset(field), little_endian);
+    case 8: return read_loc<8>(data, offset(field), little_endian);
+    }
+  }
+
+  [[nodiscard]] constexpr auto section_data() const noexcept -> std::basic_string_view<std::uint8_t> { return full_data.substr(offset(), size()); }
+
+  [[nodiscard]] constexpr auto symbol_table_num_entries() const noexcept -> std::size_t
+  {
+    if (type() == Types::SHT_SYMTAB) {
+      return size() / Symbol_Table_Entry::entry_size(bits_32);
+    } else {
+      return 0;
+    }
+  }
+
+  [[nodiscard]] constexpr auto symbol_table_entry(const std::size_t index) const noexcept -> Symbol_Table_Entry
+  {
+    const auto size = Symbol_Table_Entry::entry_size(bits_32);
+    return Symbol_Table_Entry{ bits_32, little_endian, full_data, section_data().substr(size * index, size) };
+  }
+
+  [[nodiscard]] constexpr auto symbol_table_entries() const noexcept
+  {
+    return make_iterator([this](const std::size_t idx) { return symbol_table_entry(idx); }, symbol_table_num_entries());
+  }
+
+
+
 };
 
 struct File_Header
@@ -230,17 +501,9 @@ struct File_Header
   };
 
 
-  template<typename Itr> constexpr File_Header(Itr begin, const Itr end)
-  {
-    auto outbegin = data.begin();
-    while (begin != end && outbegin != data.end()) {
-      *outbegin = *begin;
-      ++begin;
-      ++outbegin;
-    }
-  }
+  constexpr File_Header(std::basic_string_view<std::uint8_t> t_data) : data(t_data) { assert(data.size() >= 64); }
 
-  std::array<std::uint8_t, 64> data;
+  std::basic_string_view<std::uint8_t> data;
 
   // todo: move to shared code
   template<typename Itr, typename Itr2>[[nodiscard]] constexpr bool equal(Itr begin1, const Itr end1, Itr2 begin2) const noexcept
@@ -384,57 +647,48 @@ struct File_Header
   [[nodiscard]] constexpr auto section_header_num_entries() const noexcept { return read(Fields::e_shnum); }
   [[nodiscard]] constexpr auto section_header_string_table_index() const noexcept { return read(Fields::e_shstrndx); }
 
-
-  // todo, move this into a shared utility location
-  template<std::size_t Bytes>[[nodiscard]] constexpr auto read_loc(const std::size_t loc) const noexcept
+  [[nodiscard]] constexpr auto section_header(const std::size_t entry) const noexcept -> Section_Header
   {
-    // to do : assert size is within bounds?
-    if constexpr (Bytes == 1) {
-      return static_cast<std::uint8_t>(data.at(loc));
-    } else if constexpr (Bytes == 2) {
-      const std::uint16_t byte0 = data[loc];
-      const std::uint16_t byte1 = data[loc + 1];
-      if (little_endian()) {
-        return static_cast<std::uint16_t>(byte0 | (byte1 << 8));
-      } else {
-        return static_cast<std::uint16_t>((byte0 << 8) | byte1);
-      }
-    } else if constexpr (Bytes == 4) {
-      const std::uint32_t byte0 = data[loc];
-      const std::uint32_t byte1 = data[loc + 1];
-      const std::uint32_t byte2 = data[loc + 2];
-      const std::uint32_t byte3 = data[loc + 3];
-      if (little_endian()) {
-        return byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24);
-      } else {
-        return (byte0 << 24) | (byte1 << 16) | (byte2 << 8) | byte3;
-      }
-    } else if constexpr (Bytes == 8) {
-      const std::uint64_t byte0 = data[loc];
-      const std::uint64_t byte1 = data[loc + 1];
-      const std::uint64_t byte2 = data[loc + 2];
-      const std::uint64_t byte3 = data[loc + 3];
-      const std::uint64_t byte4 = data[loc + 4];
-      const std::uint64_t byte5 = data[loc + 5];
-      const std::uint64_t byte6 = data[loc + 6];
-      const std::uint64_t byte7 = data[loc + 7];
-      if (little_endian()) {
-        return byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24) | (byte4 << 32) | (byte5 << 40) | (byte6 << 48) | (byte7 << 56);
-      } else {
-        return (byte0 << 56) | (byte1 << 48) | (byte2 << 40) | (byte3 << 32) | (byte4 << 24) | (byte5 << 16) | (byte6 << 8) | byte7;
-      }
-    } else {
-      static_assert(Bytes == 1 || Bytes == 2 || Bytes == 4 || Bytes == 8);
-    }
+    assert(entry < section_header_num_entries());
+    return { bits_32(), little_endian(), data, data.substr(section_header_offset() + section_header_size() * entry) };
   }
+
+  [[nodiscard]] constexpr auto sh_string_table() const noexcept -> std::string_view
+  {
+    const auto string_header{ section_header(section_header_string_table_index()) };
+    const auto string_data{ string_header.section_data() };
+    return std::string_view{ reinterpret_cast<const char *>(string_data.data()), string_data.size() };
+  }
+
+
+
+  [[nodiscard]] constexpr auto section_headers() const noexcept
+  {
+    return make_iterator([this](const std::size_t idx) { return section_header(idx); }, section_header_num_entries());
+  }
+
+  [[nodiscard]] constexpr auto string_table() const noexcept -> std::string_view
+  {
+    const auto sh_table = sh_string_table();
+
+    for (const auto &section_header : section_headers()) {
+      if (section_header.name(sh_table) == ".strtab") {
+        const auto string_data{ section_header.section_data() };
+        return std::string_view{ reinterpret_cast<const char *>(string_data.data()), string_data.size() };
+      }
+    }
+
+    //assert(false);
+  }
+
 
   [[nodiscard]] constexpr auto read(const Fields field) const noexcept -> std::uint64_t
   {
     switch (size(field)) {
-    case 1: return read_loc<1>(offset(field));
-    case 2: return read_loc<2>(offset(field));
-    case 4: return read_loc<4>(offset(field));
-    case 8: return read_loc<8>(offset(field));
+    case 1: return read_loc<1>(data, offset(field), true);
+    case 2: return read_loc<2>(data, offset(field), little_endian());
+    case 4: return read_loc<4>(data, offset(field), little_endian());
+    case 8: return read_loc<8>(data, offset(field), little_endian());
     }
   }
 };
--- src/elf_reader.cpp
+++ src/elf_reader.cpp
@@ -4,7 +4,8 @@
 #include <fstream>
 
 // todo: move to shared location
-auto read_file(const std::filesystem::path &filename) {
+auto read_file(const std::filesystem::path &filename)
+{
   if (std::ifstream ifs{ filename, std::ios::binary }; ifs.good()) {
     const auto file_size = ifs.seekg(0, std::ios_base::end).tellg();
     ifs.seekg(0);
@@ -31,11 +32,33 @@ int main(const int argc, const char *const argv[])
 
   const auto data = read_file(filename);
 
-  const auto file_header = arm_thing::ELF::File_Header(begin(data), end(data));
+  const auto file_header = arm_thing::ELF::File_Header({ data.data(), data.size() });
 
   std::cout << "is_elf_file: " << file_header.is_elf_file() << '\n';
   std::cout << "program_header_num_entries: " << file_header.program_header_num_entries() << '\n';
   std::cout << "section_header_num_entries: " << file_header.section_header_num_entries() << '\n';
   std::cout << "section_header_string_table_index: " << file_header.section_header_string_table_index() << '\n';
 
+  const auto string_header   = file_header.section_header(file_header.section_header_string_table_index());
+  const auto sh_string_table = file_header.sh_string_table();
+
+  std::cout << "string_table_offset: " << string_header.offset() << '\n';
+  std::cout << "string_table_name_offset: " << string_header.name_offset() << '\n';
+  std::cout << "string_table_name: " << string_header.name(sh_string_table) << '\n';
+  std::cout << "string_table_size: " << string_header.size() << '\n';
+
+  std::cout << "Iterating Tables\n";
+  const auto string_table = file_header.string_table();
+  for (const auto &header : file_header.section_headers()) {
+    std::cout << "  table name: " << header.name(sh_string_table) << " offset: " << header.offset() << " size: " << header.size()
+              << " num symbol entries: " << header.symbol_table_num_entries() << '\n';
+
+    for (const auto &symbol_table_entry : header.symbol_table_entries()) {
+      std::cout << "    name_offset: " << symbol_table_entry.name_offset() << " symbol name: " << symbol_table_entry.name(string_table)
+                << " symbol offset: " << symbol_table_entry.value() << " table index: " << symbol_table_entry.section_header_table_index() << '\n';
+      if (symbol_table_entry.name(string_table) == "main") {
+        std::cout << "FOUND MAIN!\n";
+      }
+    }
+  }
 }
