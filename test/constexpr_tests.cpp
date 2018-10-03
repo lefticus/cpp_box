e3c3d1ae468ac081966fc5e94323410965725577
Razi Seyyedi <seyyedi@offis.de>
2018-10-03 14:43:29 +0200

    Change immediate value to solve Cirrus CI failure

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -325,8 +325,8 @@ TEST_CASE("Test arbitrary code")
  */
 TEST_CASE("Test memory instructions - Immediate value")
 {
-  CONSTEXPR auto systest = run_instruction(cpp_box::arm::Instruction{ 0xe59f0016 },  // ldr  r0, [pc, #22]
-                                           cpp_box::arm::Instruction{ 0xe59f1016 },  // ldr  r1, [pc, #22]
+  CONSTEXPR auto systest = run_instruction(cpp_box::arm::Instruction{ 0xe59f0016 },  // ldr  r0, [pc, #12]
+                                           cpp_box::arm::Instruction{ 0xe59f1016 },  // ldr  r1, [pc, #12]
                                            cpp_box::arm::Instruction{ 0xe5902000 },  // ldr  r2, [r0]
                                            cpp_box::arm::Instruction{ 0xe5812002 },  // str r2, [r1, #2]  address mode: offset. Store
                                            cpp_box::arm::Instruction{ 0xe5a12004 },  // str r2, [r1, #4]! address mode: pre-indexed.
