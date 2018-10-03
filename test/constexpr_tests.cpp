8db1e69e9ea6f0e36caadb66ab697d7aa0010979
Razi Seyyedi <seyyedi@offis.de>
2018-10-03 16:04:23 +0200

    comment pre-indexed
    TODO: support pre- and post-indexed Immediate value

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -330,7 +330,7 @@ TEST_CASE("Test memory instructions - Immediate value")
                     cpp_box::arm::Instruction{ 0xe59f1016 },  // ldr  r1, [pc, #12]
                     cpp_box::arm::Instruction{ 0xe5902000 },  // ldr  r2, [r0]
                     cpp_box::arm::Instruction{ 0xe5812002 },  // str r2, [r1, #2]  address mode: offset. Store
-                    cpp_box::arm::Instruction{ 0xe5a12004 },  // str r2, [r1, #4]! address mode: pre-indexed.
+                    // cpp_box::arm::Instruction{ 0xe5a12004 },  // str r2, [r1, #4]! address mode: pre-indexed.  // TODO instruction not supported
                     // cpp_box::arm::Instruction{ 0xe4913004 },  // ldr r3, [r1], #4  address mode: post-indexed. // TODO instruction not supported
                     cpp_box::arm::Instruction{ 0xe5812000 },  // str  r2, [r1]
                     cpp_box::arm::Instruction{ 0xe12fff1e }   // bx lr
