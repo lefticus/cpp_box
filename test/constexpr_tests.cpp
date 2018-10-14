bc675447748b4e3e746ef5bf91debc25dc93d0a0
Razi Seyyedi <seyyedi@offis.de>
2018-10-14 17:51:17 +0200

    Fix instruction code

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -327,7 +327,7 @@ TEST_CASE("Test arbitrary code")
  */
 TEST_CASE("Test memory instructions - Immediate value")
 {
-  CONSTEXPR auto system = run_instruction(cpp_box::arm::Instruction{ 0xe59f0016 },  // ldr  r0, [pc, #12]
+  CONSTEXPR auto system = run_instruction(cpp_box::arm::Instruction{ 0xe59f000c },  // ldr  r0, [pc, #12]
                                           cpp_box::arm::Instruction{ 0xe59f1016 },  // ldr  r1, [pc, #12]
                                           cpp_box::arm::Instruction{ 0xe5902000 },  // ldr  r2, [r0]
                                           cpp_box::arm::Instruction{ 0xe5812002 },  // str r2, [r1, #2]  address mode: offset. Store
